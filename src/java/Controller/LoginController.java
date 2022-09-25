d39e478c3ddfc9e1f63f1e5c94d4b0395a1d85b3
tuấn kiều <tuan281002@gmail.com>
2022-09-25 22:44:08 +0700

    SWP

---
+++ src/java/Controller/LoginController.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import Model.User;
+import com.mysql.cj.Session;
+import dal.Dao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author kieut
+ */
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        Dao dao = new Dao();
+        User u = dao.login(email, password);
+        if (u == null) {
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        } else {
+            HttpSession session =request.getSession();
+            session.setAttribute("user", u);
+            request.getRequestDispatcher("index.html").forward(request, response);
+        }
+    }
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/Model/Setting.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author kieut
+ */
+public class Setting {
+    int setting_id;
+    int type_id;
+    String setting_title;
+    String setting_value;
+    int display_order;
+    boolean status;
+    String description;
+    public Setting(){}
+
+    public Setting(int setting_id, int type_id, String setting_title, String setting_value, int display_order, boolean status, String description) {
+        this.setting_id = setting_id;
+        this.type_id = type_id;
+        this.setting_title = setting_title;
+        this.setting_value = setting_value;
+        this.display_order = display_order;
+        this.status = status;
+        this.description = description;
+    }
+
+    public int getSetting_id() {
+        return setting_id;
+    }
+
+    public void setSetting_id(int setting_id) {
+        this.setting_id = setting_id;
+    }
+
+    public int getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(int type_id) {
+        this.type_id = type_id;
+    }
+
+    public String getSetting_title() {
+        return setting_title;
+    }
+
+    public void setSetting_title(String setting_title) {
+        this.setting_title = setting_title;
+    }
+
+    public String getSetting_value() {
+        return setting_value;
+    }
+
+    public void setSetting_value(String setting_value) {
+        this.setting_value = setting_value;
+    }
+
+    public int getDisplay_order() {
+        return display_order;
+    }
+
+    public void setDisplay_order(int display_order) {
+        this.display_order = display_order;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Setting{" + "setting_id=" + setting_id + ", type_id=" + type_id + ", setting_title=" + setting_title + ", setting_value=" + setting_value + ", display_order=" + display_order + ", status=" + status + ", description=" + description + '}';
+    }
+}
