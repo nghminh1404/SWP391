3c163d6978b12ab15226a726fd1df0b11639e7a8
anhkthhe160643 <anhkthhe160643@fpt.edu.vn>
2022-10-06 11:27:33 +0700

    hanhcommitavatarwebcontact

--- src/java/dal/Dao.java
+++ src/java/dal/Dao.java
@@ -79,7 +79,7 @@ public class Dao extends DBContext {
             ps = con.prepareStatement(query);
             ps.setString(1, id);
             rs = ps.executeQuery();
-            if(rs.next()) {
+            if (rs.next()) {
                 return new Setting(rs.getInt(1),
                         rs.getInt(2),
                         rs.getString(3),
@@ -92,14 +92,14 @@ public class Dao extends DBContext {
         }
         return null;
     }
-    
+
     public void UpdatePesonalInfo(int user_id, String name, String mobile) {
         String sql = "update user set full_name=?, mobile=? where user_id=?;";
         try {
             con = new DBContext().connection;
             ps = con.prepareStatement(sql);
             ps.setInt(3, user_id);
-            ps.setString(1, name);            
+            ps.setString(1, name);
             ps.setString(2, mobile);
             ps.execute();
         } catch (SQLException e) {
@@ -107,40 +107,55 @@ public class Dao extends DBContext {
         }
         System.out.println("updated");
     }
-    
+
     public void UpdateAvatarURL(int user_id, String avatar_url) {
         String sql = "update User set avatar_url=? where user_id=?;";
         try {
             con = new DBContext().connection;
             ps = con.prepareStatement(sql);
             ps.setInt(2, user_id);
-            ps.setString(1, avatar_url);            
+            ps.setString(1, avatar_url);
             ps.execute();
         } catch (SQLException e) {
             System.out.println(e);
         }
         System.out.println("updated");
     }
-    
+
     public void UpdateActive(int user_id, boolean active) {
         String sql = "update User set active=? where user_id=?;";
         try {
             con = new DBContext().connection;
             ps = con.prepareStatement(sql);
             ps.setInt(2, user_id);
-            ps.setBoolean(1, active);            
+            ps.setBoolean(1, active);
             ps.execute();
         } catch (SQLException e) {
             System.out.println(e);
         }
         System.out.println("updated");
     }
-        
+
+    public void insertContact(String full_name, String email, String mobile, String message) {
+        String sql = "INSERT INTO web_contact(full_name, email, mobile, message) values(?,?,?,?)";
+        try {
+            con = new DBContext().connection;
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setNString(1, full_name);
+            ps.setString(2, email);
+            ps.setString(3, mobile);
+            ps.setNString(4, message);
+            ps.execute();
+        } catch (Exception e) {
+        }
+    }
 
     public static void main(String[] args) {
         Dao dao = new Dao();
 //        Setting u = dao.getSettingbyid("2");
-        User u = dao.login("anhnapu@mail.com", "Kkkkk1312");
-        System.out.println(u);
+//        User u = dao.login("anhnapu@mail.com", "Kkkkk1312");
+//        System.out.println(u);
+//        dao.insertContact("anh", "anh@mail.com", "085", "lalal");
+
     }
 }
