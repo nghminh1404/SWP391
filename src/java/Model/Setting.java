d12113138cbb64400c89e72e48ae6359ed62a519
unknown <nguyendan032002@gmail.com>
2022-10-15 10:09:15 +0700

    Added By Smily

--- src/java/Model/Setting.java
+++ src/java/Model/Setting.java
@@ -9,19 +9,19 @@ package Model;
  * @author kieut
  */
 public class Setting {
-     private int setting_id;
-     private int type_id;
-     private String setting_title;
-     private String setting_value;
-     private int display_order;
-     private boolean status;
-     private String description;
 
-     public Setting() {
-     }
-     
-     
-         public Setting(int setting_id, int type_id, String setting_title, String setting_value, int display_order, boolean status, String description) {
+    private int setting_id;
+    private int type_id;
+    private String setting_title;
+    private String setting_value;
+    private int display_order;
+    private boolean status;
+    private String description;
+
+    public Setting() {
+    }
+
+    public Setting(int setting_id, int type_id, String setting_title, String setting_value, int display_order, boolean status, String description) {
         this.setting_id = setting_id;
         this.type_id = type_id;
         this.setting_title = setting_title;
@@ -31,63 +31,63 @@ public class Setting {
         this.description = description;
     }
 
+    public int getSetting_id() {
+        return setting_id;
+    }
 
-     public int getSetting_id() {
-          return setting_id;
-     }
+    public void setSetting_id(int setting_id) {
+        this.setting_id = setting_id;
+    }
 
-     public void setSetting_id(int setting_id) {
-          this.setting_id = setting_id;
-     }
+    public int getType_id() {
+        return type_id;
+    }
 
-     public int getType_id() {
-          return type_id;
-     }
+    public void setType_id(int type_id) {
+        this.type_id = type_id;
+    }
 
-     public void setType_id(int type_id) {
-          this.type_id = type_id;
-     }
+    public String getSetting_title() {
+        return setting_title;
+    }
 
-     public String getSetting_title() {
-          return setting_title;
-     }
+    public void setSetting_title(String setting_title) {
+        this.setting_title = setting_title;
+    }
 
-     public void setSetting_title(String setting_title) {
-          this.setting_title = setting_title;
-     }
+    public String getSetting_value() {
+        return setting_value;
+    }
 
-     public String getSetting_value() {
-          return setting_value;
-     }
+    public void setSetting_value(String setting_value) {
+        this.setting_value = setting_value;
+    }
 
-     public void setSetting_value(String setting_value) {
-          this.setting_value = setting_value;
-     }
+    public int getDisplay_order() {
+        return display_order;
+    }
 
-     public int getDisplay_order() {
-          return display_order;
-     }
+    public void setDisplay_order(int display_order) {
+        this.display_order = display_order;
+    }
 
-     public void setDisplay_order(int display_order) {
-          this.display_order = display_order;
-     }
+    public boolean isStatus() {
+        return status;
+    }
 
-     public boolean isStatus() {
-          return status;
-     }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
-     public void setStatus(boolean status) {
-          this.status = status;
-     }
+    public String getDescription() {
+        return description;
+    }
 
-     public String getDescription() {
-          return description;
-     }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-     public void setDescription(String description) {
-          this.description = description;
-     }
-         @Override
+    @Override
     public String toString() {
         return "Setting{" + "setting_id=" + setting_id + ", type_id=" + type_id + ", setting_title=" + setting_title + ", setting_value=" + setting_value + ", display_order=" + display_order + ", status=" + status + ", description=" + description + '}';
     }
---
+++ src/java/dal/WebContactDAO.java
@@ -0,0 +1,110 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Model.*;
+
+/**
+ *
+ * @author SMILY
+ */
+public class WebContactDAO extends DBContext{
+    Connection con;
+
+    public WebContactDAO() {
+        try {
+            con = new DBContext().getConnection();
+        } catch (Exception ex) {
+            Logger.getLogger(WebContactDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void insertContact(int category_id, String full_name, String email, String mobile, String message){
+        String sql = "INSERT INTO web_contact(category_id, full_name, email, mobile, message) values(?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, category_id);
+            ps.setNString(2, full_name);
+            ps.setString(3, email);
+            ps.setString(4, mobile);
+            ps.setNString(5, message);
+            ps.execute();
+        } catch (Exception e) {
+        }
+    }
+    
+    public int getAmount(){
+        String sql = "SELECT COUNT(contact_id) FROM web_contact";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+    
+    public List<WebContact> getAll(int limit, int offset){
+        String sql = "SELECT * FROM web_contact ORDER BY contact_id limit ? offset ?";
+        List<WebContact> list = new ArrayList<>();
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, limit);
+            ps.setInt(2, offset);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                WebContact wc = new WebContact(rs.getInt(1), rs.getInt(2), rs.getInt(3), 
+                        rs.getNString(4), rs.getString(5), rs.getString(6), rs.getNString(7), rs.getNString(8));
+                list.add(wc);
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+    
+    public WebContact getContact(int contact_id){
+        String sql = "SELECT * FROM web_contact WHERE contact_id = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, contact_id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {                
+                WebContact wc = new WebContact(rs.getInt(1), rs.getInt(2), rs.getInt(3), 
+                        rs.getNString(4), rs.getString(5), rs.getString(6), rs.getNString(7), rs.getNString(8));
+                return wc;
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+    
+    public void updateWebContact(int supporter_id, String response, int contact_id){
+        String sql = "UPDATE web_contact SET supporter_id = ?, response = ? WHERE contact_id = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, supporter_id);
+            ps.setNString(2, response);
+            ps.setInt(3, contact_id);
+            ps.execute();
+        } catch (Exception e) {
+        }
+    }
+    
+    public void deleteWebContact(int contact_id){
+        String sql = "DELETE FROM web_contact WHERE contact_id = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, contact_id);
+            ps.execute();
+        } catch (Exception e) {
+        }
+    }
+    
+}
